cf700472cedd824f23f6e1f30b7c53c03d79eda6
Tsvetan Dimoff <tsdimo@gmail.com>
2024-07-25 02:09:09 +0300

    Add global configuration

---
+++ src/config.ts
@@ -0,0 +1,11 @@
+export const denominations = [
+  { value: 0.05, label: "$0.05" },
+  { value: 0.1, label: "$0.10" },
+  { value: 0.25, label: "$0.25" },
+  { value: 1, label: "$1" },
+  { value: 5, label: "$5" },
+  { value: 10, label: "$10" },
+];
+
+export const PRODUCT_API_URL =
+  "https://gist.githubusercontent.com/tdimoff/6e94de57dce160cb9c86abc294f466f8/raw/d8e2055101b1098f9194f67de84a69dbbca057f1/products.json";
